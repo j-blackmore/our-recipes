347f811bbe573fcf5f8b45be73febd410cc82d44
Joe Blackmore <joseph.blackmore@and.digital>
2019-09-24 16:08:54 +0100

    Add initial project build

---
+++ src/containers/App/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link} from "react-router-dom";
+import "typeface-roboto";
+
+function App() {
+    return (
+        <Router>
+            <div>
+                <h1>our recipes</h1>
+            </div>
+        </Router>
+    );
+}
+
+export default App;
