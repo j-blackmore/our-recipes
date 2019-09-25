4774d0be088d0d9b643b83cd79abd39f9459ff24
Joe Blackmore <joseph.blackmore@and.digital>
2019-09-25 11:25:00 +0100

    Add header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,28 @@
+import React from "react";
+import AppBar from '@material-ui/core/AppBar';
+import ToolBar from '@material-ui/core/Toolbar';
+import { Typography } from "@material-ui/core";
+import HeaderScroll from "./HeaderScroll";
+
+const appBarStyle = {
+    alignItems: 'center'
+};
+
+class Header extends React.Component {
+
+    render() {
+        return (
+        <React.Fragment>
+            <HeaderScroll>
+                <AppBar style={appBarStyle}>
+                    <ToolBar>
+                        <Typography variant="h6">Our Recipes</Typography>
+                    </ToolBar>
+                </AppBar>
+            </HeaderScroll>
+        </React.Fragment>
+    )};
+
+}
+
+export default Header;
---
+++ src/components/Header/HeaderScroll.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger'
+
+function HeaderScroll(props) {
+    const { children, window } = props;
+
+    const trigger = useScrollTrigger({
+        disableHysteresis: true,
+        threshold: 0,
+        target: window ? window() : undefined
+    });
+
+    return React.cloneElement(children, {
+        elevation: trigger ? 4 : 0,
+    });
+}
+
+export default HeaderScroll;
