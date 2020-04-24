fdb61f900847d8793ee8d3b8d95eba734e498e4b
Joe <joeblackmore97@gmail.com>
2020-04-24 16:59:03 +0100

    [tests] Clean initial react tests (#25)
    * [npm] Fix proxy in react client
    
    * [App] Remove unnecessary test file
    
    * [Header] rename test description

--- client/src/components/Header/__tests__/Header.test.js
+++ client/src/components/Header/__tests__/Header.test.js
@@ -4,8 +4,8 @@ import Header from '../Header';
 
 afterEach(cleanup);
 
-describe('Header tests', () => {
-    test('Displays header correctly', () => {
+describe('Header Component', () => {
+    test('renders correctly', () => {
         render(<Header />);
         expect(screen.getByText('Our Recipes')).toBeInTheDocument();
     });
