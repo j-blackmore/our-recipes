93025a43339873312d1b621ec4685163a53920dd
Joe Blackmore <joeblackmore97@gmail.com>
2020-05-19 16:46:20 +0100

    Add example recipes to test/utils.js

--- server/test/utils.js
+++ server/test/utils.js
@@ -16,10 +16,43 @@ const cleanupDb = async () => {
         _err => console.log('Error: could not drop database')
     );
 };
+
+const exampleRecipe1 = {
+    __v: 0,
+    _id: '53ae89df26cb170382d41fc5',
+    title: 'An example recipe',
+    subtitle: 'the example subtitle',
+    ingredients: ['1 thing', '2 lots of another'],
+    method: 'lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    extras: [''],
+    imageUrl: '',
+    imageId: '',
+    prepTime: 15,
+    cookTime: 30,
+    serves: 8,
+    creator: 'test utils',
+    createdAt: JSON.parse(JSON.stringify(new Date('2020-01-01')))
+};
+
+const exampleRecipe2 = {
+    __v: 0,
+    _id: '78ed24f619502abe58239ce6',
+    title: 'Second example recipe',
+    subtitle: 'second subtitle description',
+    ingredients: ['spoonful of one', 'something here'],
+    method: 'lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    extras: [''],
+    imageUrl: '',
+    imageId: '',
+    prepTime: 10,
+    cookTime: 32,
+    serves: 3,
+    creator: 'test utils',
+    createdAt: JSON.parse(JSON.stringify(new Date('2020-01-01')))
 };
 
 module.exports = {
     initEmptyDb: initEmptyDb,
     cleanupDb: cleanupDb,
-    exampleRecipe1: exampleRecipe1
+    exampleRecipes: [exampleRecipe1, exampleRecipe2]
 };
