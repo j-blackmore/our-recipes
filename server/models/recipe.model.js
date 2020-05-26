58fd53852a83e77c2fab5e22bce5a13359787aaa
Joe Blackmore <joeblackmore97@gmail.com>
2020-05-26 11:09:02 +0100

    Remove defaults from required fields in recipe.model

--- server/models/recipe.model.js
+++ server/models/recipe.model.js
@@ -2,18 +2,18 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-    title: { type: String, default: '', required: true },
+    title: { type: String, required: true },
     subtitle: { type: String, default: '', required: true },
-    ingredients: { type: [String], default: [''], required: true },
-    method: { type: String, default: '', required: true },
+    ingredients: { type: [String], required: true },
+    method: { type: String, required: true },
     extras: { type: [String], default: [''] },
     imageUrl: { type: String, default: '' },
     imageId: { type: String, default: '' },
-    prepTime: { type: Number, default: 0, required: true },
-    cookTime: { type: Number, default: 0, required: true },
-    serves: { type: Number, default: 0, required: true },
+    prepTime: { type: Number, required: true },
+    cookTime: { type: Number, required: true },
+    serves: { type: Number, required: true },
     createdAt: { type: Date, default: Date.now },
-    creator: { type: String, default: '', required: true }
+    creator: { type: String, default: '' }
 });
 
 module.exports = mongoose.model('Recipe', recipeSchema);
